1ee859588298bd4bbd59842e3e7f502ce392993f
pol1801 <126429609+pol1801@users.noreply.github.com>
2025-12-15 23:02:05 -0300

    4to commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://astro-tutorial-lemon.vercel.app/",
     vite: {
     server: {
       headers: {
